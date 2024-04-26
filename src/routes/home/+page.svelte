6528802401276c687dac903f5a6efa1c2390d77c
Tomatillo1 <totofarkas@gmail.com>
2024-04-26 15:01:47 +0200

    push test pb infos

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,30 @@
+<div id="navbar">
+    <button class="buttonNavbar"><img src="/navbar/halteres.png" alt=""></button>
+    <button class="buttonNavbar"><img src="/navbar/sifllet.png" alt=""></button>
+    <button class="buttonNavbar"><img src="/navbar/home.png" alt=""></button>
+    <button class="buttonNavbar"><img src="/navbar/stats.png" alt=""></button>
+    <button class="buttonNavbar"><img src="/navbar/profil.png" alt=""></button>
+</div>
+
+<style>
+    #navbar {
+        background-color: #41C1BA;
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        height: 8%;
+        position: fixed;
+        bottom: 0;
+    }
+
+    .buttonNavbar {
+        width: 20%;
+        background-color: #41C1BA;
+        border: none;
+    }
+
+    img {
+        width: 50px;
+        height: 50px;
+    }
+</style>
